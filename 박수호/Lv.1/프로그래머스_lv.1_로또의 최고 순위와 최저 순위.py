19224dbdafc7583acaa1e2e6ebc65e60aff610ae
grand7070 <grand7070@naver.com>
2022-01-01 23:53:01 +0900

    1일차:upload

---
+++ 박수호/Lv.1/프로그래머스_lv.1_로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,5 @@
+def solution(lottos, win_nums):
+    num = len(set(lottos) & set(win_nums))
+    zero_num = lottos.count(0)
+    answer = [7-num-zero_num if num != 0 else 1 if num == 0 and zero_num != 0 else 6, 7-num if num != 0 else 6]
+    return answer
