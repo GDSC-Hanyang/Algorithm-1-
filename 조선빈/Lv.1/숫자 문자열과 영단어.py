e8bb1c9448134a74e2294c1dca18fc5b8362ee1f
seonbin0319 <89207256+seonbin0319@users.noreply.github.com>
2022-01-06 14:29:37 +0900

    Create 숫자 문자열과 영단어.py

---
+++ 조선빈/Lv.1/숫자 문자열과 영단어.py
@@ -0,0 +1,24 @@
+def solution(s):
+    answer = 0
+    answers = ''
+    words = {'zero': '0',
+             'one': '1',
+             'two': '2',
+             'three': '3',
+             'four': '4',
+             'five': '5',
+             'six': '6',
+             'seven': '7',
+             'eight': '8',
+             'nine': '9'}
+    word = ''
+    for i in s:
+        if i.isdigit():
+            answers += i
+            continue
+        word += i
+        if word in words:
+            answers += words[word]
+            word = ''
+    answer = int(answers)
+    return answer
