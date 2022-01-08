9089eac1b5f582430c29cd93352b4fa60e824351
JH <jh110821@hanyang.ac.kr>
2022-01-08 23:17:39 +0900

    8일차 문제 풀이 업로드

---
+++ 홍재희/codes/8.py
@@ -0,0 +1,9 @@
+# 내적
+# 2022-01-08
+# https://programmers.co.kr/learn/courses/30/lessons/70128
+
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += a[i] * b[i]
+    return answer
