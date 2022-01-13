5cf34f86f456477005fbd3c16366cbcaa1aa2aca
JH <jh110821@hanyang.ac.kr>
2022-01-13 11:12:30 +0900

    11일차 문제 풀이 업로드

---
+++ 홍재희/codes/11.py
@@ -0,0 +1,11 @@
+# K번째 수
+# 2022-01-11
+# https://programmers.co.kr/learn/courses/30/lessons/42748
+
+def solution(array, commands):
+    answer = []
+    for cmd in commands:
+        x = array[(cmd[0] - 1):cmd[1]]
+        x.sort()
+        answer.append(x[cmd[2] - 1])
+    return answer
