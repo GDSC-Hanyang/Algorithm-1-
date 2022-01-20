64140029e17a1693560b8372011320004e796d82
grand7070 <grand7070@naver.com>
2022-01-21 03:42:27 +0900

    20일차 : upload

---
+++ 박수호/Lv.1/20일차_프로그래머스_lv.1_2016년.py
@@ -0,0 +1,8 @@
+import sys
+def solution(sizes):
+    w, h = 0, 0
+    tmp = list(map(int, sys.stdin.readline().split()))
+    enumerate
+    return answer
+
+solution([[60, 50], [30, 70], [60, 30], [80, 40]])
