46bce1d10afaffbc91233db1a29a8c1899a1415a
Suho Park <grand7070@naver.com>
2022-01-21 03:43:41 +0900

    20일차  : update

--- 박수호/Lv.1/20일차_프로그래머스_lv.1_2016년.py
+++ 박수호/Lv.1/20일차_프로그래머스_lv.1_2016년.py
@@ -1,8 +1,13 @@
-import sys
-def solution(sizes):
-    w, h = 0, 0
-    tmp = list(map(int, sys.stdin.readline().split()))
-    enumerate
+def solution(a, b):
+    day = b
+    while a > 1:
+        if a == 3: day += 29
+        elif a == 2 or a == 4 or a == 6 or a == 8 or a == 9 or a == 11:
+            day += 31
+        elif a == 5 or a == 7 or a == 10 or a == 12:
+            day += 30
+        a -= 1
+    day -= 1
+    week = day % 7
+    answer = 'FRI' if week == 0 else 'SAT' if week == 1 else 'SUN' if week == 2 else 'MON' if week == 3 else 'TUE' if week == 4 else 'WED' if week == 5 else 'THU'
     return answer
-
-solution([[60, 50], [30, 70], [60, 30], [80, 40]])
