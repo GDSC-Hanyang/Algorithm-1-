1a5f37f4d91130e7dd34bd430348e50a0d0915b7
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-01-25 17:33:05 +0900

    Create 숫자 문자열과 영단어.py

---
+++ 곽예진/숫자 문자열과 영단어.py
@@ -0,0 +1,6 @@
+dic = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'} 
+def solution(s):
+  for key,value in dic.items():
+    s=s.replace(key,value)
+    answer=s
+    return answer
