232b94bccce4c9b6a0d15cb0327be06002d1683e
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-01-29 18:02:34 +0900

    Update 6일차_없는 숫자 더하기.py

--- 곽예진/6일차_없는 숫자 더하기.py
+++ 곽예진/6일차_없는 숫자 더하기.py
@@ -1,3 +1,12 @@
 #없는 숫자 더하기
 #https://programmers.co.kr/learn/courses/30/lessons/86051
+#난이도 하
 
+def solution(numbers):
+    addsum=0
+    for i in range(10):
+        addsum += i
+    for k in numbers:
+        addsum -= k
+    answer = addsum
+    return answer
