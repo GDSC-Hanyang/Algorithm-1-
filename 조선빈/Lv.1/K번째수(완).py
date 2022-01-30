b88838399075d9285c5cf40e322190bbec029a0c
seonbin0319 <89207256+seonbin0319@users.noreply.github.com>
2022-01-31 03:18:12 +0900

    Update and rename K번째수(미완).py to K번째수(완).py

---
+++ 조선빈/Lv.1/K번째수(완).py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+
+    for i in range(len(commands)):
+        arr = array[commands[i][0]-1:commands[i][1]]
+        arr = sorted(arr)
+
+        answer.append(arr[commands[i][2]-1])
+
+    return answer
