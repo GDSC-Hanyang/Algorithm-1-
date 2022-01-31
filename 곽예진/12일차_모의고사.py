4cfc816fb3d8d895379531349129d273b6f18b93
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-01-31 13:50:58 +0900

    Update 12일차_모의고사.py

--- 곽예진/12일차_모의고사.py
+++ 곽예진/12일차_모의고사.py
@@ -38,3 +38,25 @@ def solution(answers):
         else:
             answer=[3]
     return answer
+
+##다른분 풀이
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+#enumerate는 index와 item 둘다를 나타내준다.
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
