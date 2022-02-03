c354ec63679ab87791356fe07acb5346f40bd4fc
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-02-03 20:05:35 +0900

    Create 021일차_최소직사각형.py

---
+++ 곽예진/021일차_최소직사각형.py
@@ -0,0 +1,22 @@
+#최소직사각형
+#https://programmers.co.kr/learn/courses/30/lessons/86491
+#난이도 중하
+
+def solution(sizes):
+    maxlist=[]
+    maxw=0
+    maxh=0
+    for i in sizes:
+        i.sort()
+        print(i)
+        a=i[0]
+        b=i[1]
+        if a>maxw:
+            maxw=a
+        if b>maxh:
+            maxh=b
+        
+        print(maxw,maxh)
+    answer = maxw*maxh
+    
+    return answer
