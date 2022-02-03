25f913777ab5a37f66e9582f44d6a2e83f40cd7e
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-02-03 21:50:30 +0900

    Update 023일차_부족한 금액 계산하기.py

--- 곽예진/023일차_부족한 금액 계산하기.py
+++ 곽예진/023일차_부족한 금액 계산하기.py
@@ -1,2 +1,13 @@
 #부족한 금액 계산하기
 #https://programmers.co.kr/learn/courses/30/lessons/82612
+#난이도 중하
+
+def solution(price, money, count):
+    sum=0
+    for i in range(1,count+1):
+        sum +=i
+    result=sum*price-money
+    if result<0:
+        result=0
+
+    return result
