264e4b27251c3d956b8a7c89423c4bae13b42b90
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-02-09 02:31:50 +0900

    Create 029일차_두 정수 사이의 합.py

---
+++ 곽예진/029일차_두 정수 사이의 합.py
@@ -0,0 +1,18 @@
+#두 정수 사이의 합
+#https://programmers.co.kr/learn/courses/30/lessons/12912
+#난이도 중하
+
+def solution(a, b):
+    answer = 0
+    ai=a
+    bi=b
+    if ai>bi:
+        a=bi
+        b=ai
+    if a==b:
+        answer=a
+    else:
+        for i in range(a,b+1):
+            answer += i
+    
+    return answer
