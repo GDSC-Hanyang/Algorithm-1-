9125b52bf060a743f57c2b10cca1e7829873a9dc
JH <jhsoft1108@gmail.com>
2022-02-12 15:29:51 +0900

    36일차 문제 풀이 업로드

---
+++ 홍재희/codes/36.py
@@ -0,0 +1,9 @@
+# 수박수박수박수박수박수?
+# https://programmers.co.kr/learn/courses/30/lessons/12922
+
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i % 2 == 0: answer += '수'
+        else: answer += '박'
+    return answer
