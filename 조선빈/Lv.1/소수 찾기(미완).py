c9fe365429e64bb396f615bf5b1d83f92c77e00b
seonbin0319 <89207256+seonbin0319@users.noreply.github.com>
2022-02-16 17:50:09 +0900

    Create 소수 찾기(미완).py

---
+++ 조선빈/Lv.1/소수 찾기(미완).py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    for n in range(2,n+1):
+        for i in range(2,n):
+            if n%i == 0:
+                break
+        else:
+            answer += 1
+    return answer
