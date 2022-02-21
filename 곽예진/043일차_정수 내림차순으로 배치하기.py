4e75585988347d4d611ccb37a745bbae93b4a0af
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-02-22 00:40:43 +0900

    Create 043일차_정수 내림차순으로 배치하기.py

---
+++ 곽예진/043일차_정수 내림차순으로 배치하기.py
@@ -0,0 +1,34 @@
+#정수 내림차순으로 배치하기
+#https://programmers.co.kr/learn/courses/30/lessons/12933
+
+#[1]방법. 한개씩 분리해서 sort 써서 정렬 후 다시 붙이기
+def solution(n):
+    #answer리스트에 숫자 한개씩 떼어내기
+    answer=[]
+    for i in str(int(n)):
+        answer.append(i)
+    #숫자 정렬
+    answer.sort(reverse=True)
+    #리스트 숫자 더해서 정수 변환
+    r_an=''
+    for i in answer:
+        r_an+=i
+    return int(r_an)
+    
+#[2]방법. 크기비교하면서 한개씩 붙이기-첫번째 시작점이 애매함=>별로
+
+
+# [join함수]
+#리스트에서 요소 더해서 결과 나오는 함수 = "join함수"
+
+''.join()
+
+#즉, 이렇게 코드를 짤 수도 있음.
+
+def solution(n):
+    answer=[]
+    for i in str(int(n)):
+        answer.append(i)
+    answer.sort(reverse=True)
+    #리스트 숫자 더해서 정수 변환
+    return int(''.join(answer))
