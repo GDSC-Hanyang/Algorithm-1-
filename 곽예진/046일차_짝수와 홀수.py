807112c1d9535177165d88c94a8c97ef61290657
k-yejin <91196885+k-yejin@users.noreply.github.com>
2022-02-22 01:25:18 +0900

    Create 046일차_짝수와 홀수.py

---
+++ 곽예진/046일차_짝수와 홀수.py
@@ -0,0 +1,11 @@
+#짝수와 홀수
+#https://programmers.co.kr/learn/courses/30/lessons/12937
+
+#2로 나눈 나머지가 0이냐 1이냐?
+
+def solution(num):
+    if num%2==1:
+        answer="Odd"
+    else:
+        answer="Even"
+    return answer
