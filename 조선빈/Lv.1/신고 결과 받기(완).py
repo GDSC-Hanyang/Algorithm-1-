0688170f29cc4c23e6f578596613b2e6f7c8411e
seonbin0319 <89207256+seonbin0319@users.noreply.github.com>
2022-03-08 00:08:05 +0900

    Update and rename 신고 결과 받기(미완).py to 신고 결과 받기(완).py

--- 조선빈/Lv.1/신고 결과 받기(완).py
+++ 조선빈/Lv.1/신고 결과 받기(완).py
@@ -33,3 +33,18 @@ def solution(id_list, report, k):
                     idx = id_list.index(reporting[n])
                     answer[idx] += 1       
     return answer
+
+
+##변경 코드
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
