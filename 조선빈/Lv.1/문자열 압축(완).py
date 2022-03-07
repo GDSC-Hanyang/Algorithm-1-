1def3f4f30a4669b32c601c8932d64357b959c21
seonbin0319 <89207256+seonbin0319@users.noreply.github.com>
2022-03-08 00:33:44 +0900

    Create 문자열 압축(완).py

---
+++ 조선빈/Lv.1/문자열 압축(완).py
@@ -0,0 +1,21 @@
+def solution(s):
+    num = len(s)
+    for steps in range(1, len(s)+1):
+        answer = ""
+        data = []
+        for i in range(0,len(s),steps):
+            data.append(s[i:i + steps])
+        before = data[0]
+        cnt = 1
+        for i in range(1, len(data)):
+            if before == data[i]:
+                cnt += 1
+            else:
+                tmp = str(cnt) if cnt > 1 else ""
+                answer += tmp + before
+                before = data[i]
+                cnt = 1
+        tmp = str(cnt) if cnt > 1 else ""
+        answer += tmp + before
+        num = min(num, len(answer))
+    return num
