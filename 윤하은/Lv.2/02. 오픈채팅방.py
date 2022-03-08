0247c501500ee881732722f3258835577c42f8cb
LalaYun <a47636465@gmail.com>
2022-03-08 10:27:50 +0900

    220308

--- 윤하은/Lv.2/02. 오픈채팅방.py
+++ 윤하은/Lv.2/02. 오픈채팅방.py
@@ -1,37 +1,19 @@
 def solution(record):
-    
+
     chatName = {}
-    resultTemp = []
     result = []
-    
+    temp = []
+
     for i in range(len(record)) :
         # [0] = Enter / Change / Leave, [1] = userID, [2] = name
-        temp = record[i].split(' ') 
-        
-        if temp[0] == 'Enter':
-            chatName.setdefault(temp[1])
-            chatName[temp[1]] = temp[2]
-            new = temp[1] + '님이 들어왔습니다'
-            resultTemp.append(new)
-            
-        elif temp[0] == 'Leave':
-            new = temp[1] + '님이 나갔습니다.'
-            del chatName[temp[1]]
-            resultTemp.append(new)
-            
-        elif temp[0] == 'Change':
-            chatName.pop(temp[1])
-            chatName.setdefault(temp[1])
-            chatName[temp[1]] = temp[2]
-        
-    for j in resultTemp :
-        for uid, name in chatName.items() :
-            if j[:-9] == uid :
-                answer = j.replace(uid, name)
-            #if answer[:-9] == uid :
-                if '.' not in answer :
-                    answer += '.'
-                result.append(answer)
-                break
-        
+        temp.append(record[i].split(' '))
+        if temp[i][0] != 'Leave':
+            chatName[temp[i][1]] = temp[i][2]
+
+    for j in range(len(temp)) :
+        if 'Enter' in temp[j]:
+            result.append(chatName[temp[j][1]] + '님이 들어왔습니다.')
+        if 'Leave' in temp[j]:
+            result.append(chatName[temp[j][1]] + '님이 나갔습니다.')
+
     return result
